9951258aad2ca8e0a951d71a90219b02f665f488
Mark Featherston <mark@embeddedarm.com>
2020-07-01 10:10:40 -0700

    Disable debug messages in default build

--- CORE/SERVICES/COMMON/debug_linux.h
+++ CORE/SERVICES/COMMON/debug_linux.h
@@ -36,16 +36,13 @@
     /* NOTE: the AR_DEBUG_PRINTF macro is defined here to handle special handling of variable arg macros
      * which may be compiler dependent. */
 #define AR_DEBUG_PRINTF(mask, args) do {        \
-    if (GET_ATH_MODULE_DEBUG_VAR_MASK(ATH_MODULE_NAME) & (mask)) {                    \
+    if (0) {                    \
         A_LOGGER(mask, ATH_MODULE_NAME, ATH_PRINTX_ARG args);    \
     }                                            \
 } while (0)
 #else
-    /* on non-debug builds, keep in error and warning messages in the driver, all other
-     * message tracing will get compiled out */
 #define AR_DEBUG_PRINTF(mask, args) \
-    if ((mask) & (ATH_DEBUG_ERR | ATH_DEBUG_WARN)) { A_PRINTF(ATH_PRINTX_ARG args); }
-
+    if (0) { A_PRINTF(ATH_PRINTX_ARG args); }
 #endif
 
     /* compile specific macro to get the function name string */
